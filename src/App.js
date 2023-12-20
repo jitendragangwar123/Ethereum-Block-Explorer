a532cfc8cba2deb8ed532f61ae93caf7bd7c83dd
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2023-12-20 19:58:15 +0530

    refactor:code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Alchemy, Network } from 'alchemy-sdk';
 import { useEffect, useState } from 'react';
 import { ethers } from "ethers"
 import './App.css';
+
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
